e55611df759b05ecf2be135fe5698668044a53eb
Wegt-meoh <pp1726636370@outlook.com>
2023-12-10 19:49:28 +0800

    Variables and Mutability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello world!")
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("constant is {THREE_HOURS_IN_SECONDS}");
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    //shadowing
+    let y = 5;
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("the value of y in the inner scope is: {y}");
+    }
+    println!("the value of y is {y}");
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    let mut spaces = "   ";
+    spaces = spaces.len();
 }
