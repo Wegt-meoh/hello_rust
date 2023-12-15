b86227b75c59c203e6ca526899706a58e54ad71c
Wegt-meoh <pp1726636370@outlook.com>
2023-12-15 15:04:47 +0800

    The Slice Type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
-    println!("Hello world!")
+    let mut s = String::from("hello world");
+    let w = first_world(&s);
+    println!("{w}");
+    s.clear();
+    println!("{w}");
+
+    // other slices
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
+}
+
+fn first_world(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn first_world_2(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
